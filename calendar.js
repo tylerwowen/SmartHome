75e199abf6f84a143c3cbb4c76344630a35b422b
tyler@pi <ouyang@cs.ucsb.edu>
2015-05-29 06:09:47 -0700

    first commit

---
+++ calendar.js
@@ -0,0 +1,192 @@
+var fs = require('fs');
+var readline = require('readline');
+var google = require('googleapis');
+var googleAuth = require('google-auth-library');
+var calendar = google.calendar('v3');
+var moment = require('moment');
+
+var SCOPES = ['https://www.googleapis.com/auth/calendar.readonly'];
+var TOKEN_DIR = (process.env.HOME || process.env.HOMEPATH ||
+	process.env.USERPROFILE) + '/.credentials/';
+var TOKEN_PATH = TOKEN_DIR + 'calendar-api-quickstart.json';
+
+// Load client secrets from a local file.
+fs.readFile('client_secret.json', function processClientSecrets(err, content) {
+	if (err) {
+		console.log('Error loading client secret file: ' + err);
+		return;
+	}
+	// Authorize a client with the loaded credentials, then call the Calendar API.
+	authorize(JSON.parse(content), getEvents);
+});
+
+/**
+ * Create an OAuth2 client with the given credentials, and then execute the
+ * given callback function.
+ *
+ * @param {Object} credentials The authorization client credentials.
+ * @param {function} callback The callback to call with the authorized client.
+ */
+function authorize(credentials, callback) {
+	var clientSecret = credentials.installed.client_secret;
+	var clientId = credentials.installed.client_id;
+	var redirectUrl = credentials.installed.redirect_uris[0];
+	var auth = new googleAuth();
+	var oauth2Client = new auth.OAuth2(clientId, clientSecret, redirectUrl);
+
+	// Check if we have previously stored a token.
+	fs.readFile(TOKEN_PATH, function(err, token) {
+		if (err) {
+			getNewToken(oauth2Client, callback);
+		} else {
+			oauth2Client.credentials = JSON.parse(token);
+			callback(oauth2Client);
+		}
+	});
+}
+
+/**
+ * Get and store new token after prompting for user authorization, and then
+ * execute the given callback with the authorized OAuth2 client.
+ *
+ * @param {google.auth.OAuth2} oauth2Client The OAuth2 client to get token for.
+ * @param {getEventsCallback} callback The callback to call with the authorized
+ *     client.
+ */
+function getNewToken(oauth2Client, callback) {
+	var authUrl = oauth2Client.generateAuthUrl({
+		access_type: 'offline',
+		scope: SCOPES
+	});
+	console.log('Authorize this app by visiting this url: ', authUrl);
+	var rl = readline.createInterface({
+		input: process.stdin,
+		output: process.stdout
+	});
+	rl.question('Enter the code from that page here: ', function(code) {
+		rl.close();
+		oauth2Client.getToken(code, function(err, token) {
+			if (err) {
+				console.log('Error while trying to retrieve access token', err);
+				return;
+			}
+			oauth2Client.credentials = token;
+			storeToken(token);
+			callback(oauth2Client);
+		});
+	});
+}
+
+/**
+ * Store token to disk be used in later program executions.
+ *
+ * @param {Object} token The token to store to disk.
+ */
+function storeToken(token) {
+	try {
+		fs.mkdirSync(TOKEN_DIR);
+	} catch (err) {
+		if (err.code != 'EEXIST') {
+			throw err;
+		}
+	}
+	fs.writeFile(TOKEN_PATH, JSON.stringify(token));
+	console.log('Token stored to ' + TOKEN_PATH);
+}
+
+/**
+ * Gets the next 10 events on the user's primary calendar.
+ *
+ * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
+ */
+function getEvents(auth) {
+
+	var today = moment().format('LT');
+	console.log('today: %s', today);
+	var txt = '';
+	calendar.events.list({
+		auth: auth,
+		calendarId: 'primary',
+		singleEvents: true,
+		timeMin: (new Date()).toISOString(),
+		timeMax: (new Date(new Date().getTime() + 2 * 24 * 60 * 60 * 1000)).toISOString(),
+		maxResults: 10,
+		orderBy: 'startTime'
+	}, function(err, response) {
+		if (err) {
+			console.log('There was an error contacting the Calendar service: ' + err);
+			return;
+		}
+		var events = response.items;
+		if (events.length === 0) {
+			console.log('No upcoming events found.');
+		} else {
+			var currentHour = moment().hour();
+			if (moment().hour() < 11) {
+				txt += 'Good Morning \n';
+			} else if (moment().hour() > 14 && moment().hour() < 16) {
+				txt += 'Good Afternoon \n';
+			} else if (moment().hour() > 18) {
+				txt += 'Good Evening \n';
+			} else {
+				txt += 'Hello! \n';
+			}
+
+			if (currentHour > 12) {
+				currentHour -= 12;
+			} else if (currentHour === 0) {
+				currentHour = 12;
+			}
+			txt += 'The Current time is ' + currentHour + ' ';
+			if (moment().minute() !== 0) {
+				txt += moment().minute() + ' \n';
+			}
+			txt += '. Today you have ' + events.length + ' events scheduled. \n';
+			for (var i = 0; i < events.length; i++) {
+				var event = events[i];
+				var start = event.start.dateTime; //|| event.start.date;
+				var date = new Date(start);
+				var hours = date.getHours();
+
+
+				if (hours === 0) {
+					if (date.getMinutes() !== 0) {
+						txt += 'At 12 ' + date.getMinutes() + ' am you have an event, ' + event.summary + '.\n';
+					} else {
+						txt += 'At midnight you have an event, ' + event.summary + '.\n';
+					}
+
+				} else if (hours < 12) {
+					if (date.getMinutes() !== 0) {
+						txt += 'At ' + hours + ' ' + date.getMinutes() + ' am you have an event, ' + event.summary + '.\n';
+					} else {
+						txt += 'At ' + hours + ' am you have an event, ' + event.summary + '.\n';
+					}
+				} else if (hours == 12) {
+					if (date.getMinutes() !== 0) {
+						txt += 'At ' + hour + ' ' + date.getMinutes() + ' pm you have an event, ' + event.summary + '.\n';
+					} else {
+						txt += 'At noon you have an event, ' + event.summary + '\n';
+					}
+				} else {
+					var tempHour = hours - 12;
+					if (date.getMinutes() !== 0) {
+						txt += 'At ' + tempHour + ' ' + date.getMinutes() + ' pm you have an event, ' + event.summary + '.\n';
+					} else {
+						txt += 'At ' + tempHour + ' pm you have an event, ' + event.summary + '.\n';
+					}
+				}
+
+			}
+			console.log(txt);
+			var fs = require('fs');
+			fs.writeFile("calendar.txt", txt, function(err) {
+				if (err) {
+					return console.log(err);
+				}
+
+				console.log("The file was saved! ");
+			});
+		}
+	});
+}
---
+++ news.js
@@ -0,0 +1,39 @@
+var http = require('http');
+
+//The url we want is: 'www.random.org/integers/?num=1&min=1&max=10&col=1&base=10&format=plain&rnd=new'
+var options = {
+	host: 'api.nytimes.com',
+	path: '/svc/topstories/v1/home.json?api-key=e850e4e7b13eaaccc3b2e2f30fb1f557:8:72181714'
+};
+
+callback = function(response) {
+	var str = '';
+
+	//another chunk of data has been recieved, so append it to `str`
+	response.on('data', function(chunk) {
+		str += chunk;
+	});
+
+	//the whole response has been recieved, so we just print it out here
+	response.on('end', function() {
+		var newsJson = JSON.parse(str);
+		var outputStr = 'I have fetched the following top news from New York Times for you.\n';
+		for (i = 0; i < 5; i++) {
+			outputStr += 'News ' + (i+1) + ': ' + newsJson.results[i].title + '. \n' + newsJson.results[i].abstract + '. \n';
+		}
+
+		var fs = require('fs');
+		fs.writeFile("./news.txt", outputStr, function(err) {
+			if (err) {
+				return console.log(err);
+			}
+
+			console.log("The file was saved!");
+		});
+		//console.log(str);
+	});
+
+};
+
+http.request(options, callback).end();
+
